bd5a34e496ca4451bfc0385941f1e3caf6fac758
Michael LaCroix <michael@lacroixdesign.net>
2015-12-29 14:10:19 -0600

    Update Node and Ruby environment config

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -7,6 +7,12 @@ export PATH=$PATH:$HOME/.composer/vendor/bin
 # Load dotfile binaries
 export PATH=$PATH:$DOTFILES/bin
 
+# Rubygems
+export ARCHFLAGS="-arch x86_64"
+
+# Node.js REPL history
+export NODE_REPL_HISTORY_FILE="$HOME/.node_repl_history"
+
 # chruby setup
 source /opt/boxen/env.sh
 chruby 2.1
