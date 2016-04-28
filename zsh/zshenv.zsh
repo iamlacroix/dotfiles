172cd23c98931f1c493275c46b08e4c396a01dc9
Michael LaCroix <michael@lacroixdesign.net>
2016-04-28 17:43:38 -0500

    Add nvm

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -18,6 +18,13 @@ export NODE_REPL_HISTORY_FILE="$HOME/.node_repl_history"
 export VISUAL=vim
 export EDITOR=vim
 
+# Add Homebrew lib. Added for letsencrypt
+# export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/opt/boxen/homebrew/lib"
+
 # chruby setup
 source /opt/boxen/env.sh
 chruby 2.1
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+. "$(brew --prefix nvm)/nvm.sh"
